8dc339c48945ef20630871506a900ffbccaa8cf9
allXgaming <mrperfectguide.in@gmail.com>
2026-04-22 09:25:13 +0530

    Add comment for CORS request handling
    Added a comment to indicate that requests from all domains are accepted.

--- api/cors.js
+++ api/cors.js
@@ -1,4 +1,5 @@
 module.exports = (req, res) => {
+  // সব ডোমেইন থেকে রিকোয়েস্ট গ্রহণ করবে
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
